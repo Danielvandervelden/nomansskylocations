08356abf47d46b838c3b72f9ffcba72ac2283178
danielvandervelden <daniel@42functions.nl>
2019-04-03 22:09:29 +0200

    chosenType is now set to type, much easier.

--- components/UI/NmsButton.vue
+++ components/UI/NmsButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<nuxt-link :class="color" v-if="checkProps" tag="a" :to="link" class="button"><slot /></nuxt-link>
-	<button :class="color" class="button" v-else-if="chosenType == 'submit'" :type="chosenType"><slot /></button>
+	<button :class="color" class="button" v-else-if="type == 'submit'" :type="type"><slot /></button>
 	<a :class="color" v-else-if="back" @click="$router.back()" class="back-button button"><slot /></a>
 	<a :class="color" :href="extLink" target="_blank" class="button" v-else><slot /></a>
 </template>
@@ -9,7 +9,7 @@
 	export default {
 		props: [
 			'link',
-			'chosenType',
+			'type',
 			'extLink',
 			'color',
 			'back'
