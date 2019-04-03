3ac7f9ed561a21c1a1af9ddbbc227b0b594328fa
danielvandervelden <daniel@42functions.nl>
2019-04-03 22:12:03 +0200

    Result page now displays multitools correctly! Planets and ships still need to be displayed properly!

---
+++ pages/search/results/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="nms-search-results">
+		<div class="nms-search-results__wrapper">
+			<h1 class="nms-search-results__title">{{getSearchResults.title}}</h1>
+			<nms-search-results :data="getSearchResults.results"></nms-search-results>
+		</div>
+	</div>
+</template>
+
+<script>
+	import NmsSearchResults from "@/components/custom/NmsSearchResults.vue";
+	export default {
+		components: {
+			'nms-search-results': NmsSearchResults
+		},
+		computed: {
+			getSearchResults() {
+				console.log(this.$store.getters['search/search/getResults'].results);
+				return this.$store.getters['search/search/getResults'];
+			}
+		}
+	}
+</script>
+<style scoped lang='scss'>
+	.nms-search-results {
+		padding: 2rem;
+		box-sizing: border-box;
+
+		&__wrapper {
+			padding: 3rem;
+			box-sizing: border-box;
+			@include faded-bg();
+		}
+
+		h1 {
+			font-size: 2rem;
+			text-align: center;
+		}
+	}
+</style>
--- store/search/search.js
+++ store/search/search.js
@@ -1,26 +1,85 @@
 import { db, storage } from '@/plugins/database/firebase';
 
 export const state = () => ({
-	searchQuery: null
+	searchResults: {
+		title: '',
+		results: []
+	},
+	postData: null
+
 })
 
 export const getters = {
-	
+	getResults: state => {
+		return state.searchResults;
+	},
 }
 
 export const mutations = {
-	
+	setSearchState(state, {postData, searchData}) {
+		this._vm.loading(false);
+		state.searchResults.results = postData;
+		if(searchData.searchType === "multitools") {
+			state.searchResults.title = `Search results for: ${searchData.rank} class ${searchData.type} multitool w/ ${searchData.slots} slots`;
+		}
+		this.$router.push('/search/results');
+	}
 }
 
 export const actions = {
 	async submitSearch({dispatch, commit}, searchData) {
 		if(searchData.searchType == "multitools") {
 			let snapshot = await db.collection('posts/multitools/' + searchData.type).get()
-			console.log(snapshot.docs.map(doc => doc.data()));
+			.then(snapshot => {
+				let postArray = [] 
+				snapshot.forEach(doc => {
+					if(
+						doc.data().class == searchData.rank &&
+						doc.data().galaxy == searchData.galaxy &&
+						doc.data().slots == searchData.slots
+					) {
+						postArray.push(doc.data());
+					}
+				});
+
+				commit('setSearchState', {postData: postArray, searchData: searchData});
+			})
+
 		} else if(searchData.searchType == "ships") {
+			let snapshot = await db.collection('posts/multitools/' + searchData.type).get()
+			.then(snapshot => {
+				let postArray = [] 
+				snapshot.forEach(doc => {
+					if(
+						doc.data().class == searchData.rank &&
+						doc.data().galaxy == searchData.galaxy &&
+						doc.data().slots == searchData.slots
+					) {
+
+						postArray.push(doc.data());
+					}
+				});
+
+				commit('setSearchState', {postData: postArray, searchData: searchData});
+			})
 
 		} else if(searchData.searchType == "planets") {
-			
+			let snapshot = await db.collection('posts/multitools/' + searchData.biome).get()
+			.then(snapshot => {
+				let postArray = [] 
+				snapshot.forEach(doc => {
+					if(
+						doc.data().class == searchData.rank &&
+						doc.data().galaxy == searchData.galaxy &&
+						doc.data().slots == searchData.slots
+					) {
+
+						postArray.push(doc.data());
+					}
+				});
+
+				commit('setSearchState', {data: postArray, searchData: searchData});
+			})
 		}
 	}
 }
