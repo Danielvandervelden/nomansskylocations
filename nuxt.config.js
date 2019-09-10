ba2199b75380fb1702bffc3db1c4f9cd0b121283
danielvandervelden <daniel@42functions.nl>
2019-09-10 16:00:53 +0200

    Fixed production and dev things.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,129 +1,140 @@
 const pkg = require('./package')
 
-
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: pkg.description
-      }
-    ],
-    link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      {
-        rel: "stylesheet",
-        href: "https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-      },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Work+Sans:300,900"
-	  }
-    ],
-  },
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#fff'
-  },
-
-  /*
-   ** Global CSS
-   */
-  css: [
-    "~assets/styles/_base.scss"
-  ],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-	{ src: '~plugins/GlobalComponents.js' },
-	{ src: '~plugins/GlobalFunctions.js' },
-	{ src: '~plugins/GlobalFilters.js' },
-	{ src: '~plugins/database/firebase.js' }
-  ],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-	'@nuxtjs/pwa',
-	'@nuxtjs/style-resources',
-	'cookie-universal-nuxt'
-  ],
-
-  /*
-   ** Global SCSS loader
-   */
-  styleResources: {
-	scss: ['~assets/styles/_base.scss']
-  },
-
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-	widthCredentials: false
-  },
-
-  /*
-   ** Build configuration
-   */
-  build: {
-	extractCSS: true,
-	babel: {
-		presets: [
-			["@babel/preset-env", {
-				useBuiltIns: 'usage',
-				corejs: '3.0.0',
-				targets: {
-					ie: 11,
-					browsers: 'last 2 versions'
-				}
-			}],
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: pkg.name,
+		meta: [{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: pkg.description
+			}
 		],
-		plugins: [
-			["@babel/plugin-syntax-dynamic-import"],
-			["@babel/plugin-transform-runtime", {
-				"regenerator": true
-			}]
-		]
+		link: [{
+				rel: 'icon',
+				type: 'image/x-icon',
+				href: '/favicon.ico'
+			},
+			{
+				rel: "stylesheet",
+				href: "https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+			},
+			{
+				rel: "stylesheet",
+				href: "https://fonts.googleapis.com/css?family=Work+Sans:300,900"
+			}
+		],
 	},
-  	/*
-  	 ** You can extend webpack config here
-  	 */
-  	extend(config, ctx) {
 
-  	}
-  },
-
-  env: {
-		  
-  	},
-
-  	transition: {
-  		name: 'fade',
-  		mode: 'out-in'
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: {
+		color: '#fff'
 	},
-  }
+
+	/*
+	 ** Global CSS
+	 */
+	css: [
+		"~assets/styles/_base.scss"
+	],
+
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [{
+			src: '~plugins/GlobalComponents.js'
+		},
+		{
+			src: '~plugins/GlobalFunctions.js'
+		},
+		{
+			src: '~plugins/GlobalFilters.js'
+		},
+		{
+			src: '~plugins/database/firebase.js'
+		}
+	],
+
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa',
+		'@nuxtjs/style-resources',
+		'cookie-universal-nuxt'
+	],
+
+	/*
+	 ** Global SCSS loader
+	 */
+	styleResources: {
+		scss: ['~assets/styles/_base.scss']
+	},
+
+	/*
+	 ** Axios module configuration
+	 */
+	axios: {
+		widthCredentials: false
+	},
+
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		extractCSS: true,
+		babel: {
+			presets: [
+				["@babel/preset-env", {
+					useBuiltIns: 'usage',
+					corejs: '3.0.0',
+					targets: {
+						ie: 11,
+						browsers: 'last 2 versions'
+					}
+				}],
+			],
+			plugins: [
+				["@babel/plugin-syntax-dynamic-import"],
+				["@babel/plugin-transform-runtime", {
+					"regenerator": true
+				}]
+			]
+		},
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {
+
+		}
+	},
+
+	env: {
+
+	},
+
+	transition: {
+		name: 'fade',
+		mode: 'out-in'
+	},
+
+	server: {
+		port: process.env.NODE_ENV === 'production' ? 80 : 8000,
+		host: process.env.NODE_ENV === 'production' ? '167.71.73.18' : 'localhost'
+	}
+}
